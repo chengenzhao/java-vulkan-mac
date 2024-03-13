c3aa422a677338a880c4606bea19858d240b21cf
chengenzhao <chengen.zhao@mail.mcgill.ca>
2024-03-14 00:20:15 +0800

    refactor

--- src/main/java/com/example/HelloApplication.java
+++ src/main/java/com/example/HelloApplication.java
@@ -66,7 +66,7 @@ public class HelloApplication extends HelloApplication1{
       ARENA = arena;
 
       var pInstance = createVkInstance(arena, DEBUG);
-      var instance = pInstance.get(C_POINTER, 0);//similar to & operator in C, like &pInstance
+      var instance = pInstance.get(C_POINTER, 0);
 
       List<String> extensions = getAvailableExtensions(arena);
       System.out.println("Available extensions:");
